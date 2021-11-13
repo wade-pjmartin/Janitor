d9f2b35f6c02cf664dc7ab19b182219d1bcff4de
wade-pjmartin <70186676+wade-pjmartin@users.noreply.github.com>
2021-11-13 08:02:40 -0500

    Created Courier, A Dynamic Web App for Distributing Information

---
+++ Courier.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Courier.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
